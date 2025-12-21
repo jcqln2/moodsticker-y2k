e3199813418bf5445430e0e6da34986e8502d71d
jcqln2 <jacqueline.connect@gmail.com>
2025-12-21 18:38:36 -0500

    Update mood.js so it works on prod

--- public/js/mood.js
+++ public/js/mood.js
@@ -1,7 +1,7 @@
 // /public/js/mood.js
 
 // ==================== CONFIGURATION ====================
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = window.location.origin + '/api';
 
 // ==================== STATE ====================
 let moods = [];
