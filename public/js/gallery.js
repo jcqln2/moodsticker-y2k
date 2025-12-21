6f3f42517a91a47ff329d33723ded76701111772
jcqln2 <jacqueline.connect@gmail.com>
2025-12-21 18:41:06 -0500

    Update gallery.js

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,7 +1,7 @@
 // /public/js/gallery.js
 
 // ==================== CONFIGURATION ====================
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = window.location.origin + '/api';
 
 // ==================== STATE ====================
 let allStickers = [];
